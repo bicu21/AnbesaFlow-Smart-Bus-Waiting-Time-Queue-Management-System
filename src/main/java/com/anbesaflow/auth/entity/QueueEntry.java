74676a918dcaaad40e1078cead041218600a889e
Eyob <ekassaye0461@gmail.com>
2026-06-30 14:30:35 +0300

    create QueueEntry entity and migration

--- src/main/java/com/anbesaflow/auth/entity/QueueEntry.java
+++ src/main/java/com/anbesaflow/auth/entity/QueueEntry.java
@@ -1,5 +1,82 @@
 package com.anbesaflow.auth.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "queue_entries")
 public class QueueEntry {
-    w
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer position;
+
+    @Column(nullable =false)
+    private LocalDateTime joinedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String busStop;
+
+    public QueueEntry() {
+    }
+
+    public QueueEntry(Long id,
+                      Integer position,
+                      LocalDateTime joinedAt,
+                      User user,
+                      String busStop) {
+        this.id = id;
+        this.position = position;
+        this.joinedAt = joinedAt;
+        this.user = user;
+        this.busStop = busStop;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public LocalDateTime getJoinedAt() {
+        return joinedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getBusStop() {
+        return busStop;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public void setJoinedAt(LocalDateTime joinedAt) {
+        this.joinedAt = joinedAt;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setBusStop(String busStop) {
+        this.busStop = busStop;
+    }
 }
--- src/main/resources/db/migration/V2__create_queue_entry_table.sql
+++ src/main/resources/db/migration/V2__create_queue_entry_table.sql
@@ -1,10 +1,23 @@
-CREATE TABLE queue_entry (
+CREATE TABLE queue_entries (
+
     id BIGSERIAL PRIMARY KEY,
-    position INT NOT NULL,
+
+    position INTEGER NOT NULL,
+
     joined_at TIMESTAMP NOT NULL,
 
-    user_id BIGINT NOT NULL,
     bus_stop VARCHAR(100) NOT NULL,
 
-    CONSTRAINT fk_queue_user FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id BIGINT NOT NULL UNIQUE,
+
+    CONSTRAINT fk_queue_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
 );
+
+CREATE INDEX idx_queue_bus_stop
+ON queue_entries(bus_stop);
+
+CREATE INDEX idx_queue_position
+ON queue_entries(position);
